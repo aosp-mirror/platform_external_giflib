fd7010216ff6061a03d23bc5879e2b314c3158ed
Bert Münnich <bmuennich@users.sourceforge,net>
2013-01-06 01:05:19 -0500

    Fix misdecoding when a GIF overflows the code table.
    Version 4.1.6 and 4.2.1 do not decode this image correctly: http://ge.tt/15VJnNB/v/52
    
    This is caused by the fact that DGifDecompressLine() overwrites the
    last entry of the code table again and again in the case that the
    table is full.
    
    This issue was originally reported here:
    https://github.com/muennich/sxiv/issues/75
    
    All regression tests pass with this change.

--- lib/dgif_lib.c
+++ lib/dgif_lib.c
@@ -871,7 +871,7 @@ DGifDecompressLine(GifFileType *GifFile, GifPixelType *Line, int LineLen)
                 while (StackPtr != 0 && i < LineLen)
                     Line[i++] = Stack[--StackPtr];
             }
-            if (LastCode != NO_SUCH_CODE) {
+            if (LastCode != NO_SUCH_CODE && Prefix[Private->RunningCode - 2] == NO_SUCH_CODE) {
                 Prefix[Private->RunningCode - 2] = LastCode;
 
                 if (CrntCode == Private->RunningCode - 2) {
