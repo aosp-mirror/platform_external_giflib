e189ac98c1d4e339fd0327aae046f908e22fa1bc
Matt Sarett <msarett@google.com>
2015-08-21 11:23:19 -0400

    Update giflib to 5.1.1
    BUG:23351000
    
    Change-Id: I7641388fe78c5e66f4ffc178b76411dcb79f4e8a

--- Android.mk
+++ Android.mk
@@ -4,11 +4,14 @@ include $(CLEAR_VARS)
 LOCAL_SDK_VERSION := 9
 
 LOCAL_SRC_FILES := \
-	dgif_lib.c \
-	gifalloc.c \
-	gif_err.c
+    dgif_lib.c     \
+    egif_lib.c     \
+    gifalloc.c     \
+    gif_err.c      \
+    gif_hash.c     \
+    quantize.c
 
-LOCAL_CFLAGS += -Wno-format -DHAVE_CONFIG_H
+LOCAL_CFLAGS += -Wno-format -Wno-sign-compare -Wno-unused-parameter -DHAVE_CONFIG_H
 LOCAL_SDK_VERSION := 8
 LOCAL_MODULE:= libgif
 
