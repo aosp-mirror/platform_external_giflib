40c3f62aba3398f0f727361e36b60c5daf0fd035
Eric S. Raymond <esr@thyrsus.com>
2016-03-15 08:50:25 -0400

    Addtress SF bug #84: DGifSlurp returns error return code.

--- lib/dgif_lib.c
+++ lib/dgif_lib.c
@@ -768,7 +768,7 @@ DGifSetupDecompress(GifFileType *GifFile)
     BitsPerPixel = CodeSize;
 
     /* this can only happen on a severely malformed GIF */
-    if (BitsPerPixel > 8 || Private->RunningBits > 32) {
+    if (BitsPerPixel > 8) {
 	GifFile->Error = D_GIF_ERR_READ_FAILED;	/* somewhat bogus error code */
 	return GIF_ERROR;    /* Failed to read Code size. */
     }
