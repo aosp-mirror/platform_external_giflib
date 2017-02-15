3e76914fc9e21039f94875741851c2439300a225
Eric S. Raymond <esr@thyrsus.com>
2017-02-15 08:46:11 -0500

    Re-entrancy fix.

--- lib/gif_font.c
+++ lib/gif_font.c
@@ -237,7 +237,8 @@ GifDrawBoxedText8x8(SavedImage *Image,
 		      border + TextWidth * GIF_FONT_WIDTH + border - 1,
 		      border + LineCount * GIF_FONT_HEIGHT + border - 1, bg);
 	(void)strcpy(dup, (char *)legend);
-	cp = strtok((char *)dup, "\r\n");
+	char *lasts;
+	cp = strtok_r(dup, "\r\n", &lasts);
 	do {
 	    int leadspace = 0;
 
@@ -246,7 +247,7 @@ GifDrawBoxedText8x8(SavedImage *Image,
 
 	    GifDrawText8x8(Image, x + border + (leadspace * GIF_FONT_WIDTH),
 			   y + border + (GIF_FONT_HEIGHT * i++), cp, fg);
-	    cp = strtok((char *)NULL, "\r\n");
+	    cp = strtok_r(NULL, "\r\n", &lasts);
 	} while (cp);
 	(void)free((void *)dup);
 
